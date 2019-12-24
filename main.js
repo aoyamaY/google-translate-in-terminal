e84c9438602e3e8073991b7fc8b0037129d0a782
gavin.yuan <gavin.yuan@trantect.com>
2019-12-24 17:56:19 +0800

    save work

--- main.js
+++ main.js
@@ -1,18 +1,34 @@
-const https = require('https');
+const superagent = require('superagent');
 const Entites = require('html-entities').XmlEntities;
 const entites = new Entites();
 
 const sourceText = entites.encode(process.argv.slice(2).join(' '));
-// console.log(sourceText)
-// console.log(process.argv)
-https.get(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=zh-CN&dt=t&q=${sourceText}`, (res) => {
-    let data = '';
-    res.on('data', (chunk) => {
-        data += chunk;
-    });
-    res.on('end', () => {
-        console.log(JSON.parse(data)[0][0]);
-    });
-}).on('error', (err) => {
-    console.log(err);
-});
+
+superagent
+  .get('https://translate.googleapis.com/translate_a/single')
+  .query({
+    client: 'gtx',
+    sl: 'auto',
+    tl: 'zh-CN',
+    dt: 't',
+    q: sourceText
+  })
+  .end((err, res) => {
+    if (err) { console.log(err); }
+    const result = res.body[0].map(element => element[0]).join(' ');
+    console.log(res.body[0][0][0])
+    console.log(result);
+  })
+
+// https.get(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=zh-CN&dt=t&q=${sourceText}`, (res) => {
+//     let data = '';
+//     res.on('data', (chunk) => {
+//         data += chunk;
+//     });
+//     res.on('end', () => {
+//         const result = JSON.parse(data)[0].map(element => element[0]).join(' ');
+//         console.log(result);
+//     });
+// }).on('error', (err) => {
+//     console.log(err);
+// });
