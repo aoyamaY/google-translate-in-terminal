5db2506c2ed01fe99aa4884836f600f2ba9b72cd
gavin.yuan <gavin.yuan@trantect.com>
2020-01-14 11:02:48 +0800

    finish index.js as entry

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const translate = require('./lib/translate');
-const setting = require('./lib/menu')
 const fs = require('fs');
+const setting = require('./lib/setting');
+const translate = require('./lib/translate');
 
 const rawdata = fs.readFileSync('config.json')
-const { sourcelanguage, targetlanguage} = JSON.parse(rawdata);
-const sourceText = process.argv.slice(2).join(' ');
+const config = JSON.parse(rawdata);
 
-const print = async () => {
-    const translated = await translate(sourcelanguage, targetlanguage, sourceText);
-    console.log(translated)
-}
-setting()
-// print()
+const main = async () => {
+  if (process.argv[2] === '--setting') {
+    setting(config);
+  } else {
+    const sourceText = process.argv.slice(2).join(' ');
+    const translated = await translate(config.sourcelanguage, config.targetlanguage, sourceText);
+    console.log(`\nTranslation:\n${translated}`);
+  }
+};
+
+main();
